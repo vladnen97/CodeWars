bfeccab40e0f862fc313862f946cc9dfcc7d187c
vladnen97 <darkdescent1991@gmail.com>
2023-01-04 23:18:03 +0300

    Find The Parity Outlier

---
+++ 6kyu/findTheParityOutlier.js
@@ -0,0 +1,20 @@
+/*Find The Parity Outlier*/
+
+/*
+You are given an array (which will have a length of at least 3, but could be very large) containing integers.
+ The array is either entirely comprised of odd integers or entirely comprised of even integers except for a single integer N.
+ Write a method that takes the array as an argument and returns this "outlier" N.
+ */
+
+function findOutlier(integers){
+    let odds = [], evens = [];
+    integers.forEach( num => num % 2 === 0 ? evens.push(num) : odds.push(num));
+    return evens.length === 1 ? evens[0] : odds[0];
+}
+
+
+
+
+
+console.log(findOutlier([2, 4, 0, 100, 4, 11, 2602, 36]));
+console.log(findOutlier([160, 3, 1719, 19, 11, 13, -21]));
